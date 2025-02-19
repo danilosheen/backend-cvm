2ccc10476a465ae237b7dbc10fb19e762f36a96d
danilosheen <c.danilo.f.silva@gmail.com>
2025-02-19 02:05:01 -0300

    up

--- src/dist/app.dev.js
+++ src/dist/app.dev.js
@@ -4,10 +4,9 @@ var express = require("express");
 
 var cors = require("cors");
 
-var app = express();
-
 var pdfRoutes = require("./routes/pdfOrcamento");
 
+var app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/pdf", pdfRoutes);
