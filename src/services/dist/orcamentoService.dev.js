5d1f2f865563893f13aa92540b98c23bcbadfba2
danilosheen <c.danilo.f.silva@gmail.com>
2025-02-24 22:16:28 -0300

    ajustando o pupperteer para rodar na vercel

--- src/services/dist/orcamentoService.dev.js
+++ src/services/dist/orcamentoService.dev.js
@@ -8,8 +8,12 @@ var puppeteer = require("puppeteer");
 
 var handlebars = require("handlebars");
 
+var puppeteerCore = require("puppeteer-core");
+
+var chromium = require("@sparticuz/chromium-min");
+
 function createPDF(nomeCliente, telefoneContato, pacoteViagem, localSaida, dataSaida, horaSaida, dataRetorno, horaRetorno, valor, modeloVan, valorAcrescimoKm) {
-  var data, templateHtml, template, html, browser, page, pdfBuffer;
+  var data, templateHtml, template, html, browser, executablePath, page, pdfBuffer;
   return regeneratorRuntime.async(function createPDF$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
@@ -25,62 +29,85 @@ function createPDF(nomeCliente, telefoneContato, pacoteViagem, localSaida, dataS
             dataRetorno: dataRetorno,
             horaRetorno: horaRetorno,
             valor: valor,
-            modeloVan: modeloVan,
-            valorAcrescimoKm: valorAcrescimoKm
+            modeloVan: modeloVan || "Van Mercedes minibus com 20 lugares, ar-condicionado, bancos reclinaveis e som",
+            valorAcrescimoKm: valorAcrescimoKm || "4,25"
           };
-          data.modeloVan == "" ? data.modeloVan = "Van Mercedes minibus com 20 lugares, ar-condicionado, bancos reclinaveis e som" : data.modeloVan;
-          data.valorAcrescimoKm == "" ? data.valorAcrescimoKm = "4,25" : data.valorAcrescimoKm; // Ler o template corretamente
-
           templateHtml = fs.readFileSync(path.join(__dirname, "../templates/orcamento.html"), "utf8");
           template = handlebars.compile(templateHtml);
           html = template(data);
-          _context.next = 9;
+
+          if (!(process.env.AWS_LAMBDA_FUNCTION_VERSION || process.env.VERCEL || process.env.RENDER)) {
+            _context.next = 14;
+            break;
+          }
+
+          _context.next = 8;
+          return regeneratorRuntime.awrap(chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar'));
+
+        case 8:
+          executablePath = _context.sent;
+          _context.next = 11;
+          return regeneratorRuntime.awrap(puppeteerCore.launch({
+            executablePath: executablePath,
+            args: chromium.args,
+            headless: chromium.headless,
+            defaultViewport: chromium.defaultViewport
+          }));
+
+        case 11:
+          browser = _context.sent;
+          _context.next = 17;
+          break;
+
+        case 14:
+          _context.next = 16;
           return regeneratorRuntime.awrap(puppeteer.launch({
             args: ["--no-sandbox"],
             headless: true
           }));
 
-        case 9:
+        case 16:
           browser = _context.sent;
-          _context.next = 12;
+
+        case 17:
+          _context.next = 19;
           return regeneratorRuntime.awrap(browser.newPage());
 
-        case 12:
+        case 19:
           page = _context.sent;
-          _context.next = 15;
+          _context.next = 22;
           return regeneratorRuntime.awrap(page.setContent(html, {
             waitUntil: "networkidle0"
           }));
 
-        case 15:
-          _context.next = 17;
+        case 22:
+          _context.next = 24;
           return regeneratorRuntime.awrap(page.pdf({
             format: "A4",
             printBackground: true
           }));
 
-        case 17:
+        case 24:
           pdfBuffer = _context.sent;
-          _context.next = 20;
+          _context.next = 27;
           return regeneratorRuntime.awrap(browser.close());
 
-        case 20:
+        case 27:
           return _context.abrupt("return", pdfBuffer);
 
-        case 23:
-          _context.prev = 23;
+        case 30:
+          _context.prev = 30;
           _context.t0 = _context["catch"](0);
           console.error("Erro ao gerar PDF:", _context.t0);
           throw _context.t0;
 
-        case 27:
+        case 34:
         case "end":
           return _context.stop();
       }
     }
-  }, null, null, [[0, 23]]);
-} // ⚠️ Certifique-se de exportar corretamente a função
-
+  }, null, null, [[0, 30]]);
+}
 
 module.exports = {
   createPDF: createPDF
--- src/services/orcamentoService.js
+++ src/services/orcamentoService.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const puppeteer = require("puppeteer");
 const handlebars = require("handlebars");
+const puppeteerCore = require("puppeteer-core")
+const chromium  = require("@sparticuz/chromium-min")
 
 async function createPDF(
   nomeCliente,
@@ -27,15 +29,10 @@ async function createPDF(
       dataRetorno,
       horaRetorno,
       valor,
-      modeloVan,
-      valorAcrescimoKm
+      modeloVan: modeloVan || "Van Mercedes minibus com 20 lugares, ar-condicionado, bancos reclinaveis e som",
+      valorAcrescimoKm: valorAcrescimoKm || "4,25"
     };
 
-    data.modeloVan == "" ? data.modeloVan = "Van Mercedes minibus com 20 lugares, ar-condicionado, bancos reclinaveis e som" : data.modeloVan;
-    data.valorAcrescimoKm == "" ? data.valorAcrescimoKm = "4,25" : data.valorAcrescimoKm;
-    
-
-    // Ler o template corretamente
     const templateHtml = fs.readFileSync(
       path.join(__dirname, "../templates/orcamento.html"),
       "utf8"
@@ -43,15 +40,25 @@ async function createPDF(
     const template = handlebars.compile(templateHtml);
     const html = template(data);
 
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox"],
-      headless: true,
-    });
+    let browser;
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION || process.env.VERCEL || process.env.RENDER) {
+      const executablePath = await chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar')
+            browser = await puppeteerCore.launch({
+                executablePath,
+                args: chromium.args,
+                headless: chromium.headless,
+                defaultViewport: chromium.defaultViewport
+            });
+    } else {
+      browser = await puppeteer.launch({
+        args: ["--no-sandbox"],
+        headless: true,
+      });
+    }
 
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
 
-    // Gerar PDF diretamente no buffer
     const pdfBuffer = await page.pdf({
       format: "A4",
       printBackground: true,
@@ -65,5 +72,4 @@ async function createPDF(
   }
 }
 
-// ⚠️ Certifique-se de exportar corretamente a função
 module.exports = { createPDF };
