6618780c732fbeb474520330dd18fcca063f6cb2
danilosheen <c.danilo.f.silva@gmail.com>
2025-05-13 17:42:27 -0300

    atualização da rota de /saldo-anterior

--- src/routes/saldoAnteriorRoutes.js
+++ src/routes/saldoAnteriorRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const saldoAnteriorController = require("../controllers/saldoAnteriorController");
 
-router.post("/saldo-restante", authMiddleware, saldoAnteriorController.create);
-router.get("/saldo-restante", authMiddleware, saldoAnteriorController.read);
-router.put("/saldo-restante/:id", authMiddleware, saldoAnteriorController.update);
-router.delete("/saldo-restante/:id", authMiddleware, saldoAnteriorController.delete);
+router.post("/saldo-anterior", authMiddleware, saldoAnteriorController.create);
+router.get("/saldo-anterior", authMiddleware, saldoAnteriorController.read);
+router.put("/saldo-anterior/:id", authMiddleware, saldoAnteriorController.update);
+router.delete("/saldo-anterior/:id", authMiddleware, saldoAnteriorController.delete);
 
 module.exports = router;
